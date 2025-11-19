33fa9400af70fb85d0905707df64d257a4787a05
3bbaas <ah.abbas333@gmail.com>
2025-11-19 16:06:58 +0200

    hmm, now we can start dance

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,28 @@
 import express from 'express';
+import config from '@/configs';
+import routes from '@/routes';
 
 const app = express();
 
-app.listen()
+(async () => {
+  try {
+    app.use('/api/v1', routes);
+    app.listen(config.PORT, () => {
+      console.log(`-> START: Server Running: http://localhost:${config.PORT}`);
+    });
+  } catch (err) {
+    console.log('-> FAILURE: Failed to start the server, ', err);
+  }
+})();
+
+const handleServerShutdown = async () => {
+  try {
+    console.log('-> SHUTDOWN: Server shutdown.');
+    process.exit(0);
+  } catch (err) {
+    console.log('-> ERROR: Server shutdown with error, ', err);
+  }
+};
+
+process.on('SIGTERM', handleServerShutdown);
+process.on('SIGINT', handleServerShutdown);
