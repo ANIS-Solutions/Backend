94967cb0f3af37c7791b8ba00cdd178312ac85a0
3bbaas <ah.abbas333@gmail.com>
2025-11-21 16:22:12 +0200

    refactor: add space wronge

--- src/server.ts
+++ src/server.ts
@@ -10,11 +10,12 @@ const app = express();
   try {
     // dotenv.config({ path: './config.env' });
     app.use(morgan('dev'));
-        app.use('/api/v1', routes);
+    app.use('/api/v1', routes);
+        console.log(process.env.PORT);
         console.log(process.env.PORT);
     app.listen(config.PORT, () => {
-      console.log(  
-          `-> START: Server Running: http://localhost:${config.PORT}/api/v1`,
+      console.log(
+        `-> START: Server Running: http://localhost:${config.PORT}/api/v1`,
       );
     });
   } catch (err) {
