63adda7f7706eb0918d793d73bca767365b64197
Ahmed Abbas <ah.abbas333@gmail.com>
2026-04-08 02:48:09 +0200

    feat: many fixes, apply design patterns, clean arch and more and moreeee

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,8 +2,6 @@ import { RuleConfigSeverity, type UserConfig } from '@commitlint/types';
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
-  parserPreset: 'conventional-changelog-atom',
-  formatter: '@commitlint/format',
   rules: {
     'type-enum': [
       RuleConfigSeverity.Error,
